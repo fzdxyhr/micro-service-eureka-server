462dcc50a8b021b211d5d439bf38a7c8a28ee2c7
yhr <1361579931@qq.com>
2018-03-25 23:13:10 +0800

    代码提交

---
+++ src/main/java/com/microservice/microserviceeurekaserver/EurekaServerConfig.java
@@ -0,0 +1,13 @@
+package com.microservice.microserviceeurekaserver;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by YHR on 2018/3/25.
+ */
+
+@EnableEurekaServer
+@Configuration
+public class EurekaServerConfig {
+}
---
+++ src/main/java/com/microservice/microserviceeurekaserver/MicroServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.microserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MicroServiceEurekaServerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceEurekaServerApplication.class, args);
+	}
+}
